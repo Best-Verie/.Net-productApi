e2efa94a0a90a4646531af2ff8e10e3b027176da
Best-Verie <irabestverie2016@gmail.com>
2022-08-24 09:28:14 +0200

    feat: adds controller

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductApi.model;
+
+namespace ProductApi.Controllers
+{
+
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ProductController : Controller
+    {
+        private static List<Product> _products = new List<Product>
+    {
+        new Product
+        {
+            id=1,
+            title="Inyange",
+            description="flavoured milk",
+            cost=3000
+        },
+        new Product
+        {
+            id=2,
+            title="x-product",
+            description="flavoured milk",
+            cost=3000
+        },
+
+        new Product
+        {
+            id=3,
+            title="Ikiryo",
+            description="flavoured milk",
+            cost=3000
+        },
+
+    };
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> getProduct()
+        {
+            return Ok(_products);
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<List<Product>>> createProduct(Product product)
+        {
+            _products.Add(product);
+            return Ok(_products);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Product>> updateProduct(Product product)
+        {
+            var _prod = _products.Find(p => p.id == product.id);
+            if (_prod == null)
+            {
+                return BadRequest("Product not found!");
+            }
+
+            _prod.title = product.title;
+            _prod.description = product.description;
+            _prod.cost = product.cost;
+
+            return Ok(_prod);
+        }
+    }
+}
---
+++ model/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductApi.model
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public int cost { get; set;}
+
+        
+    }
+}
